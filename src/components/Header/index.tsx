ec3c80ea2f4516cd67fab72d73c5bb0641a3057e
LuizEduardoSC <luizeduardoedd1@gmail.com>
2025-11-18 10:35:03 -0300

    fix: cetralizar logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import * as S from './styles'
 import heroImg from '../../assets/Background.svg'
 import LogoImg from '../../assets/Logo.svg'
 import { useLocation, Link } from 'react-router-dom'
+import { Container } from '../../styles'
 
 const Header = () => {
   const location = useLocation()
@@ -10,26 +11,28 @@ const Header = () => {
 
   return (
     <S.Hero isHome={isHome} style={{ backgroundImage: `url(${heroImg})` }}>
-      {isPerfil ? (
-        <S.HeaderPerfil>
-          <Link to="/">
-            <S.RestaurantesLink>Restaurantes</S.RestaurantesLink>
-          </Link>
-          <Link to="/">
+      <Container>
+        {isPerfil ? (
+          <S.HeaderPerfil>
+            <Link to="/">
+              <S.RestaurantesLink>Restaurantes</S.RestaurantesLink>
+            </Link>
+            <Link to="/">
+              <S.Logo src={LogoImg} alt="Logo" />
+            </Link>
+            <S.CarrinhoLink>0 produto(s) no carrinho</S.CarrinhoLink>
+          </S.HeaderPerfil>
+        ) : (
+          <S.HomeContent>
             <S.Logo src={LogoImg} alt="Logo" />
-          </Link>
-          <S.CarrinhoLink>0 produto(s) no carrinho</S.CarrinhoLink>
-        </S.HeaderPerfil>
-      ) : (
-        <>
-          <S.Logo src={LogoImg} alt="Logo" />
-          {isHome && (
-            <S.PageTitle>
-              Viva experiências gastronômicas <br /> no conforto da sua casa
-            </S.PageTitle>
-          )}
-        </>
-      )}
+            {isHome && (
+              <S.PageTitle>
+                Viva experiências gastronômicas <br /> no conforto da sua casa
+              </S.PageTitle>
+            )}
+          </S.HomeContent>
+        )}
+      </Container>
     </S.Hero>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,37 +9,39 @@ export const Hero = styled.div<HeroProps>`
   width: 100%;
   height: ${(props) => (props.isHome ? '384px' : '186px')};
   display: flex;
-  flex-direction: column;
   justify-content: center;
-  align-items: center;
   background-repeat: no-repeat;
   background-size: cover;
   position: relative;
+  padding: 40px 0;
 `
 
 export const Logo = styled.img`
   width: 125px;
   height: 57.5px;
-  margin-top: 40px;
 `
 
 export const PageTitle = styled.h1`
   font-weight: 900;
   font-size: 36px;
-  letter-spacing: 0;
   text-align: center;
   margin-top: 140px;
   color: ${cores.vermelho};
+
+  @media (max-width: 768px) {
+    font-size: 24px;
+    margin-top: 80px;
+  }
 `
 
 export const HeaderPerfil = styled.div`
   max-width: 1024px;
   width: 100%;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+  position: relative;
   padding: 0 16px;
-  margin-top: 40px;
 
   a {
     text-decoration: none;
@@ -47,6 +49,8 @@ export const HeaderPerfil = styled.div`
 `
 
 export const RestaurantesLink = styled.span`
+  position: absolute;
+  left: 16px;
   font-size: 18px;
   font-weight: 900;
   color: ${cores.vermelho};
@@ -59,6 +63,8 @@ export const RestaurantesLink = styled.span`
 `
 
 export const CarrinhoLink = styled.span`
+  position: absolute;
+  right: 16px;
   font-size: 18px;
   font-weight: 900;
   color: ${cores.vermelho};
@@ -68,4 +74,13 @@ export const CarrinhoLink = styled.span`
   &:hover {
     opacity: 0.8;
   }
+`
+
+export const HomeContent = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
 `
